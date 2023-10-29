e6ca7dbe6026b54e19af19ca7301846cec9534fd
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2023-10-30 11:36:51 +1300

    initial commit

---
+++ client/script.js
@@ -0,0 +1,105 @@
+const form = document.querySelector("form")
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval;
+
+function loader(el){
+    el.textContent = '';
+
+    loadInterval = setInterval(() => {
+        el.textContent += '.'
+
+        if (el.textContent.length > 4){
+            el.textContent = ' ' 
+        }
+    }, 300)
+}
+
+function typeText(el, text){
+    let index = 0;
+
+    let interval = setInterval(() => {
+        if(index < text.length){
+            el.innerHTML += text.charAt(index) //charAt is a javascript string method used to retrieve a character at a specific index.
+            index++
+        } else{
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+function generateUniqueId(){
+    const timestamp = Date.now();
+    const randomNum = Math.random();
+    const hex = randomNum.toString(16)
+
+    return `id-${timestamp}-${hex}`
+}
+
+function chatStripe(isAi, value, uniqueId){
+    //add to profile class <img src="${isAI ? bot : user}" alt="${isAi ? 'bot' : 'user'}"/>
+    return(
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+        `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    // user's chatstripe 
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    form.reset();
+
+    //bot chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId)
+
+    loader(messageDiv)
+
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if(response.ok){
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        typeText(messageDiv, parsedData)
+    } else{
+        const err = await response.text();
+
+        messageDiv.innterHTML="Something went wrong..."
+        alert(err)
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13){
+        handleSubmit(e)
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,56 @@
+import express from "express"; 
+import * as dotenv from "dotenv"; //allows communication with dotenv file
+import cors from "cors"; //cross origin requests
+import OpenAI from "openai";
+
+dotenv.config()
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.get('/', async(req,res) => {
+    res.status(200).send({
+        message:'Hi it me, Belle',
+    })
+})
+
+app.post('/', async (req, res) => {
+    try {
+      const prompt = req.body.prompt;
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            "role": "user",
+            "content": `${prompt}`
+          }
+        ],
+        temperature: 0.69,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      });
+  
+      if (response && response.choices && response.choices.length > 0) {
+        const botResponse = response.choices[0].message.content;
+        res.status(200).send({ bot: botResponse });
+      } else {
+        console.log("Invalid response structure:", response);
+        res.status(500).send({ error: "Invalid API response" });
+      }      
+    } catch (error) {
+      console.log("API request error:", error);
+      console.log("API Response:", response);
+      res.status(500).send({ error: "API request error" });
+    }
+  });
+  
+
+app.listen(5000, () => console.log("App is running on port http://localhost:5000"))
