6cc276ea9fc698acb809c18c3a7bc922f56d1e48
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2023-10-31 12:19:17 +1300

    refactor ui

--- client/script.js
+++ client/script.js
@@ -1,3 +1,6 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
 const form = document.querySelector("form")
 const chatContainer = document.querySelector('#chat_container')
 
@@ -43,6 +46,10 @@ function chatStripe(isAi, value, uniqueId){
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
                 <div class="profile">
+                <img 
+                src=${isAi ? bot : user} 
+                alt="${isAi ? 'bot' : 'user'}" 
+              />
                 </div>
                 <div class="message" id=${uniqueId}>${value}</div>
             </div>
