0e89fc8d83e66d9832ce843b0de8a90b72d96f2c
IsobellaJohnson <64507442+IsobellaJohnson@users.noreply.github.com>
2023-11-03 11:54:54 +1300

    rm js file

--- client/index.js
+++
@@ -1,4 +0,0 @@
-function toggleInfo() {
-    var infoContent = document.querySelector('.info-content');
-    infoContent.style.display = infoContent.style.display === 'block' ? 'none' : 'block';
-  }   
